4226d0a59f2893e572d2d078a81a63ef44ea2da8
Bhaskar <sbhaskarr43@gmail.com>
2026-03-04 22:32:37 +0530

    Initial React API project

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error: {error}</h2>;
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>User List</h1>
+
+      {users.map((user) => (
+        <div
+          key={user.id}
+          style={{
+            border: "1px solid #ccc",
+            padding: "10px",
+            marginBottom: "10px",
+            borderRadius: "6px",
+          }}
+        >
+          <h3>{user.name}</h3>
+          <p>Email: {user.email}</p>
+          <p>Company: {user.company.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
